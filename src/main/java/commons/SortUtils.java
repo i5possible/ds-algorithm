ea75d782130cce61bc79c74718fb89443a362c2a
shangxiaobin <18092456631@163.com>
2017-06-11 16:35:35 +0800

    create the class SortUtils for some basic function,such as exchange the values of a array

---
+++ src/main/java/commons/SortUtils.java
@@ -0,0 +1,24 @@
+package commons;
+
+/**
+ * 
+ * @author shangxiaobin on 11/06/2017.
+ *
+ */
+public class SortUtils {
+	
+	
+	/**
+	 * this method is used for exchanging values of a array
+	 * @param input  
+	 * @param left
+	 * @param right
+	 */
+	public static <T> void exchangeArrayValue(T[] input,int left,int right) {
+		T temp = null;
+		temp = input[left];
+		input[left] = input[right];
+		input[right] = temp;
+	}
+
+}
