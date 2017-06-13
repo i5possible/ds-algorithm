1223772a2ea8c03d528ac13600091f15ab73a1c0
Liang Hong <lianghongbuaa@gmail.com>
2017-06-13 22:36:42 +0800

    use paraeterized test

--- src/test/java/sorting/SorterTest.java
+++ src/test/java/sorting/SorterTest.java
@@ -1,34 +1,50 @@
 package sorting;
 
+import commons.SortUtils;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
-import javax.smartcardio.Card;
+import java.util.Arrays;
+import java.util.Collection;
 
 /**
  * Created by lianghong on 11/06/2017.
  */
+@RunWith(Parameterized.class)
 public class SorterTest {
-    private Integer[] input = {2, 1, 4, 5, 3};
-    private Integer[] expected = {1, 2, 3, 4, 5};
 
-    private Person lh = new Person("Liang Hong", 25, new Car(0));
-    private Person sxb = new Person("Shang Xiaobin", 24, new Car(100));
-    private Person cl = new Person("Chen Liang", 47, new Car(200));
-    private Person[] personInput = {lh, sxb, cl};
-    private Person[] personExpected = {cl, lh, sxb};
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        Integer[] input = {2, 1, 4, 5, 3};
+        Integer[] expected = {1, 2, 3, 4, 5};
 
-    @Test
-    public void bubble_sort_test() {
-        Sorter<Integer> sorter = new BubbleSorter<>();
-        Integer[] actual = sorter.sort(input);
-        Assert.assertArrayEquals(expected, actual);
+        Person lh = new Person("Liang Hong", 25, new Car(0));
+        Person sxb = new Person("Shang Xiaobin", 24, new Car(100));
+        Person cl = new Person("Chen Liang", 47, new Car(200));
+        Person[] personInput = {lh, sxb, cl};
+        Person[] personExpected = {cl, lh, sxb};
+
+        return Arrays.asList(new Object[][]{
+                {new BubbleSorter<Integer>(), input, expected},
+                {new BubbleSorter<Person>(), personInput, personExpected}
+        });
+    }
+
+    private Sorter sorter;
+
+    private Comparable[] input;
+    private Comparable[] expected;
+
+    public SorterTest(Sorter sorter, Comparable[] input, Comparable[] expected) {
+        this.sorter = sorter;
+        this.input = input;
+        this.expected = expected;
     }
 
     @Test
-    public void bubble_sort_object_test() {
-        Sorter<Person> sorter = new BubbleSorter<>();
-        Person[] actual = sorter.sort(personInput);
-        Assert.assertArrayEquals(personExpected, actual);
+    public void test() {
+        Assert.assertArrayEquals(expected, sorter.sort(input));
     }
 }
